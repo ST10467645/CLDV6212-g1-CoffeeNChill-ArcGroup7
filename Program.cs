5c6a701af0ffd7f45dade6ddc1d1b52fed702852
ST10472683 <ST10472683@myemeris.edu.za>
2026-08-25 14:07:51 +0200

    chore: confirm repo acccess and local setup

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
 
+//ST10472683 (Kristen keve) testing clone and pushing to repo.
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
